faf3027154ec329abddb34526ed0fcef94a05709
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-29 10:15:18 +0530

    29th May

---
+++ Multiplication Table For Number/script.js
@@ -0,0 +1,7 @@
+function multiTable(number) {
+  return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    .map((item) => `${item} * ${number} = ${item * number}`)
+    .join("\n");
+}
+
+console.log(multiTable(5));
