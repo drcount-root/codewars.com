21827dc0401fe70c74b325fddf6f8fea76dbc698
Subham Sahu <about.subhamsahu@gmail.com>
2022-06-04 20:10:19 +0530

    4cx-4

---
+++ Using TypeScript/[4 kyu]/Sum Strings as Numbers/script.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.sumStrings = void 0;
+var sumStrings = function sumStrings(a, b) {
+    return +a + +b + ""; // if a = "456667" then +a = 456667
+};
+exports.sumStrings = sumStrings;
+console.log((0, exports.sumStrings)("712569312664357328695151392", "8100824045303269669937"));
+console.log(typeof (0, exports.sumStrings)("712569312664357328695151392", "8100824045303269669937"));
--- Using TypeScript/[4 kyu]/Sum Strings as Numbers/script.ts
+++ Using TypeScript/[4 kyu]/Sum Strings as Numbers/script.ts
@@ -1,8 +1,11 @@
 export const sumStrings = function sumStrings(a: string, b: string): string {
-  return BigInt(a) + BigInt(b) + "";
+  return +a + +b + ""; // if a = "456667" then +a = 456667
 };
 
-console.log(sumStrings("1", "2"));
 console.log(
   sumStrings("712569312664357328695151392", "8100824045303269669937")
+);
+
+console.log(
+  typeof sumStrings("712569312664357328695151392", "8100824045303269669937")
 );
